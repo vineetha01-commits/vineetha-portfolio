342f3e1e25da30362386aae46af115ec192ddc2a
Vineetha Budige <vinithabudige0410@gmail.com>
2026-07-16 19:30:51 +0530

    Simplify footer section design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
 import "../styles/Footer.css";
 
 import {
-  FaLinkedin,
-  FaInstagram,
-  FaHeart,
-  FaReact
+  FaReact,
+  FaHeart
 } from "react-icons/fa";
+
 
 function Footer() {
 
@@ -13,46 +12,17 @@ function Footer() {
 
     <footer className="footer">
 
-      <h3>
-        Vineetha Budige
-      </h3>
-
-      <p>
-        Electronics & Communication Engineer • Software Testing • Creative Explorer
-      </p>
-
-
-      <div className="footer-icons">
-
-        <a
-          href="https://www.linkedin.com/in/vineetha-budige-19654b219"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="LinkedIn"
-        >
-          <FaLinkedin />
-        </a>
-
-
-        <a
-          href="https://www.instagram.com/vinitha._.goud/"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Instagram"
-        >
-          <FaInstagram />
-        </a>
-
-      </div>
-
-
       <div className="footer-bottom">
 
         <p>
           © 2026 Vineetha Budige. Built with
+          
           <FaReact className="react-icon" />
+
           React
+
           <FaHeart className="heart-icon" />
+
         </p>
 
       </div>
@@ -62,5 +32,6 @@ function Footer() {
   );
 
 }
+
 
 export default Footer;
